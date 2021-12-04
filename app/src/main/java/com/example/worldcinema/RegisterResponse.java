8d018c9f5947d92af5ea8b97d0a4d6bbe321b854
ocean88 <Myworldid88>
2021-12-04 13:55:38 +0900

    123

---
+++ app/src/main/java/com/example/worldcinema/RegisterResponse.java
@@ -0,0 +1,13 @@
+package com.example.worldcinema;
+
+public class RegisterResponse {
+    public int code;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
